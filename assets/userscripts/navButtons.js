fcec62a74fd67c5076beee065fe260cbc22b97f7
Nightdavisao <nightdavisao27@protonmail.com>
2025-04-27 10:30:21 -0300

    prepend back button

--- assets/userscripts/navButtons.js
+++ assets/userscripts/navButtons.js
@@ -4,8 +4,8 @@
     function createSVG(paths, viewBox) {
       const svg = document.createElementNS(SVG_NS, "svg");
       svg.setAttribute("fill", "currentColor");
-      svg.setAttribute("width", "20");
-      svg.setAttribute("height", "20");
+      svg.setAttribute("width", "18");
+      svg.setAttribute("height", "18");
       svg.setAttribute("viewBox", viewBox)
       paths.forEach(pathD => {
         const path = document.createElementNS(SVG_NS, "path");
@@ -51,21 +51,24 @@
         ],
         '0 0 24 24'
       )
+      burgerMenuBtn.style.paddingTop = '2px'
       burgerMenuBtn.addEventListener('click', () => window.AMWrapper.openBurgerMenu())
       const backBtn = createButton(
         ['M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'],
         "-128 0 512 512"
       )
+      backBtn.style.paddingTop = '2px'
       backBtn.addEventListener('click', () => window.history.back());
-      const forwardBtn = createButton(
-        ['M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'],
-        '-128 0 512 512'
-      )
-      forwardBtn.addEventListener('click', () => window.history.forward())
+      // const forwardBtn = createButton(
+      //   ['M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'],
+      //   '-128 0 512 512'
+      // )
+      // forwardBtn.addEventListener('click', () => window.history.forward())
 
-      container.appendChild(backBtn);
+      //container.appendChild(backBtn);
       container.appendChild(burgerMenuBtn)
       //container.appendChild(forwardBtn)
+      logoHeader.prepend(backBtn)
       logoHeader.appendChild(container);
   
       return true;
